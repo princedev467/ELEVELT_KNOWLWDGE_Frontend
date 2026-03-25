b35c1d7b94b9c778f22a295d1dab814095c96cc3
prince476-web <princemovaliya467@gmail.com>
2026-03-25 17:48:22 +0530

    efrfgrfg

--- src/utility/url.js
+++ src/utility/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL="http://localhost:2022/api/v1/";
+export const BASE_URL="https://elevelt-knowlwdge-backend.vercel.app/api/v1/";
 
-export const IMAGE_URL="http://localhost:2022/";
+export const IMAGE_URL="https://elevelt-knowlwdge-backend.vercel.app/";
