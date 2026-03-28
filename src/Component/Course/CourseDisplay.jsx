b213c326782face8d06bad6097988a40104c36f2
prince476-web <princemovaliya467@gmail.com>
2026-03-28 19:31:17 +0530

    sda

--- src/Component/Course/CourseDisplay.jsx
+++ src/Component/Course/CourseDisplay.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Grid, Box } from '@mui/material';
+import { useGetCourseQuery } from '../../Redux/Api/Course.Api';
 
 function CourseDisplay(props) {
+  
+    const [search, setSearch]=useState('')
+
+    
+        const { data } = useGetCourseQuery(); //get Data
+        console.log("course",data);
+        
   return (
     <div>
       <h2>hello sdfj</h2>
