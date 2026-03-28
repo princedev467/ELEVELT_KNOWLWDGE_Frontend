04f0225c360c2463ab99edf8fc7be143e800f98f
prince476-web <princemovaliya467@gmail.com>
2026-03-28 19:36:57 +0530

    courseDisplay

--- src/Component/Course/CourseDisplay.jsx
+++ src/Component/Course/CourseDisplay.jsx
@@ -9,11 +9,69 @@ function CourseDisplay(props) {
     
         const { data } = useGetCourseQuery(); //get Data
         console.log("course",data);
+
+        let course=data.data
         
+         let  courseFilter
+   
+ if (search.trim() !== "") {
+    courseFilter = course?.filter(v =>
+        v.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    let filterCourse=search?courseFilter:course
   return (
-    <div>
-      <h2>hello sdfj</h2>
-    </div>
+    <Box sx={{ paddingTop: 5, paddingLeft: 22, paddingBottom: 5 }}>
+          <div>
+            <input type="text" onChange={(e)=>{setSearch(e.target.value)}}   placeholder="Search..."
+            value={search} style={{backgroundColor:'#24292e' ,color:'#fff'}}/>
+          </div>
+          <br /><br />
+          {
+             <Grid container spacing={4} >
+            {filterCourse?.map((v) => (
+    
+              <Card
+                sx={{
+                  width: '280px',
+                  justifyContent: 'space-between',
+    
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  width='280px'
+                  height="100"
+                  image={`${v.course_img.url}`}
+                  alt={v.name}
+                />
+    
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6" fontWeight="bold" gutterBottom>
+                    {v.name}
+                  </Typography>
+    
+                  <Typography variant="body2" color="text.secondary">
+                    {v.description}
+                  </Typography>
+                </CardContent>
+    
+                <CardActions sx={{ justifyContent: 'space-between', px: 2, paddingBottom: 2 }}>
+                  <Button size="small" variant="outlined">
+                    Learn More
+                  </Button>
+                  <Button size="small" variant="contained">
+                    Buy
+                  </Button>
+                </CardActions>
+              </Card>
+    
+            ))}
+          </Grid>
+          }
+          
+        </Box>
   );
 }
 
