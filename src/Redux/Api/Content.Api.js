89367318ab14af455e0727c75e2fb02ada99dc81
prince476-web <princemovaliya467@gmail.com>
2026-04-28 17:27:33 +0530

    create rtk for content

---
+++ src/Redux/Api/Content.Api.js
@@ -0,0 +1,43 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { BASE_URL } from '../../utility/url'
+
+// Define a service using a base URL and expected endpoints
+export const contentApi = createApi({
+  reducerPath: 'contentApi',
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    getcontent: builder.query({
+      query: () => 'content/getAllContent',
+      providesTags: ['content']
+    }),
+    addcontent: builder.mutation({
+      query: (data) => ({
+        url: 'content/addContent',
+        method: 'POST',
+        body: data
+      }),
+      invalidatesTags:['content']    
+    }),
+    updatecontent: builder.mutation({
+      query: (data) => ({
+        url: `content/updateContent/${data._id}`,
+        method: 'PUT',
+        body: data
+      }),
+      invalidatesTags:['content']
+    }),
+    deletecontent: builder.mutation({
+      query: (id) => ({
+        url: `content/deleteContent/${id}`,
+        method: 'DELETE',
+        body: id
+      }),
+      invalidatesTags: ['content']
+    }),
+   
+  })
+})
+
+
+export const {useGetcontentQuery,useAddcontentMutation,useUpdatecontentMutation,useDeletecontentMutation} = contentApi
--- src/Routes/PrivateRouts.jsx
+++ src/Routes/PrivateRouts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-function PrivateRouts(props) {
+function PrivateRouts(props) {  
   let auth = true;
 
   return (
